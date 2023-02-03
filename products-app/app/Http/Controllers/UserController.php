b5599beb2c1cc4e41b9e2d33a4f3489327d6df58
Maria Paula Hernandez <mpaulah24@gmail.com>
2023-02-02 20:58:30 -0500

    Crud User controller

--- products-app/app/Http/Controllers/UserController.php
+++ products-app/app/Http/Controllers/UserController.php
@@ -18,14 +18,21 @@ class UserController extends Controller
                 'password' =>'required|string',
             ]);
 
-            User::create([
+            $user = User::create([
                 'name' => $request->name,
                 'lastname' => $request->lastname,
                 'email' => $request->email,
                 'username' => $request->username,
                 'password' => Hash::make($request->password),
-                'role' => 2,
             ]);
+
+            if ($request->admin_code && $request->admin_code == '123456') {
+                $user->role = 1;
+                $user->save();
+            } else {
+                $user->role = 2;
+                $user->save();
+            }
 
             return response()->json([
                'message' =>'Usuario creado exitosamente'
@@ -35,6 +42,91 @@ class UserController extends Controller
             return response()->json([
                 'message' => $e->getMessage()
             ], 400);
+        }
+    }
+
+    public function index() {
+        try {
+            $users = User::all();
+
+            if(count($users) == 0){
+                return response()->json();           
+            }
+
+            return response()->json([
+                'users' => $users
+            ],200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 400);
+        }
+    }
+
+    public function show($id){
+        try {
+            $user = User::find($id);
+
+            if (!$user){
+                return response()->json([
+                    'error' => 'User not found'
+                ],404);
+            }
+
+            return response()->json([
+                'user' => $user
+            ],200);
+            
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage
+            ],400);
+        }
+    }
+
+    public function update($id, Request $request){
+        try {
+            $user = User::find($id);
+
+            if(!$user){
+                return response()->json([
+                    'error' => 'User not found'
+                ],404);
+            }
+
+            $user->update($request->all());
+
+            return response()->json([
+                'message' => 'User updated successfully'
+            ],200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage
+            ],400);
+        }
+    }
+
+    public function destroy($id){
+        try {
+            $user = User::find($id);
+
+            if(!$user){
+                return response()->json([
+                    'error' => 'User not found'
+                ],404);
+            }
+
+            $user->delete();
+
+            return response()->json([
+                'message' => 'User deleted successfully'
+            ],200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage
+            ],400);
         }
     }
 }
