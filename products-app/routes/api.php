4af10846f08d3526718995219e44f483fe388168
Maria Paula Hernandez <mpaulah24@gmail.com>
2023-02-02 20:58:51 -0500

    Routes, products and orders

--- products-app/routes/api.php
+++ products-app/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AuthController;
 
 
@@ -21,32 +22,42 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/hello',function(){
-    return "Hello world";
+//Users 
+Route::group([
+    'prefix' => 'users'
+], function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::post('/', [UserController::class, 'register']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
 });
 
-//Registro de usuarios
-Route::post('/user/register', [UserController::class, 'register']);
-
+//Products
 Route::group([
+    'prefix' => 'products'
+], function () {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::get('/{id}', [ProductController::class, 'show']);
+    Route::post('/', [ProductController::class, 'create']);
+    Route::put('/{id}', [ProductController::class, 'update']);
+    Route::delete('/{id}', [ProductController::class, 'destroy']);
+});
 
-    'middleware' => 'auth',
+//Login 
+
+Route::post('login', [AuthController::class, 'login']);
+
+//Autenticación de usuarios
+Route::group([
+    'middleware' => 'auth:api',
     'prefix' => 'auth'
-
 ], function ($router) {
-
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh',[AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'me']);
-    Route::get('hello', function () {
-        return "Hello World";
-    });
 });
 
-Route::group([
-    'prefix' => 'auth'
-], function () {
-    Route::post('login', [AuthController::class, 'login']);
-});
+
 
 
