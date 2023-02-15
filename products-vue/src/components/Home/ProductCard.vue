bdefa5041a17acca8fe95a42bd25d59241278e3e
Maria Paula Hernandez <mpaulah24@gmail.com>
2023-02-15 16:28:42 -0500

    Create Product Component

---
+++ products-vue/src/components/Home/ProductCard.vue
@@ -0,0 +1,167 @@
+<template>
+  <div class="row">
+    <div class="col-lg-4 col-md-12 mb-4">
+      <div class="card" style="border-radius: 15px">
+        <div
+          class="bg-image hover-overlay ripple ripple-surface ripple-surface-light"
+          data-mdb-ripple-color="light"
+        >
+          <img
+            src="https://media.istockphoto.com/id/1221677097/photo/make-up-cosmetics-products-against-pink-color-background.jpg?b=1&s=170667a&w=0&k=20&c=C3sbcRcACM5E7sxFkON61OR5qqFvm8gHs8vwsFN6Nuc="
+            style="border-top-left-radius: 15px; border-top-right-radius: 15px"
+            class="img-fluid"
+            alt="Laptop"
+          />
+          <a href="#!">
+            <div class="mask"></div>
+          </a>
+        </div>
+        <div class="card-body pb-0">
+          <div class="d-flex justify-content-between">
+            <div>
+              <p><a href="#!" class="text-dark">Dell Xtreme 270</a></p>
+              <p class="small text-muted">Laptops</p>
+            </div>
+            <div>
+              <div
+                class="d-flex flex-row justify-content-end mt-1 mb-4 text-danger"
+              >
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+              <p class="small text-muted">Rated 4.0/5</p>
+            </div>
+          </div>
+        </div>
+        <hr class="my-0" />
+        <div class="card-body pb-0">
+          <div class="d-flex justify-content-between">
+            <p><a href="#!" class="text-dark">$3,999</a></p>
+            <p class="text-dark">#### 8787</p>
+          </div>
+          <p class="small text-muted">VISA Platinum</p>
+        </div>
+        <hr class="my-0" />
+        <div class="card-body">
+          <div
+            class="d-flex justify-content-between align-items-center pb-2 mb-1"
+          >
+            <a href="#!" class="text-dark fw-bold">Cancel</a>
+            <button type="button" class="btn btn-dark">Buy now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-md-12 mb-4">
+      <div class="card" style="border-radius: 15px">
+        <div
+          class="bg-image hover-overlay ripple ripple-surface ripple-surface-light"
+          data-mdb-ripple-color="light"
+        >
+          <img
+            src="https://img.freepik.com/premium-photo/alginate-clay-face-masks-cosmetic-products-masks-creams-jars-top-view-flat-lay_73253-899.jpg"
+            style="border-top-left-radius: 15px; border-top-right-radius: 15px"
+            class="img-fluid"
+            alt="Laptop"
+          />
+          <a href="#!">
+            <div class="mask"></div>
+          </a>
+        </div>
+        <div class="card-body pb-0">
+          <div class="d-flex justify-content-between">
+            <div>
+              <p><a href="#!" class="text-dark">Dell Xtreme 270</a></p>
+              <p class="small text-muted">Laptops</p>
+            </div>
+            <div>
+              <div
+                class="d-flex flex-row justify-content-end mt-1 mb-4 text-danger"
+              >
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+              <p class="small text-muted">Rated 4.0/5</p>
+            </div>
+          </div>
+        </div>
+        <hr class="my-0" />
+        <div class="card-body pb-0">
+          <div class="d-flex justify-content-between">
+            <p><a href="#!" class="text-dark">$3,999</a></p>
+            <p class="text-dark">#### 8787</p>
+          </div>
+          <p class="small text-muted">VISA Platinum</p>
+        </div>
+        <hr class="my-0" />
+        <div class="card-body">
+          <div
+            class="d-flex justify-content-between align-items-center pb-2 mb-1"
+          >
+            <a href="#!" class="text-dark fw-bold">Cancel</a>
+            <button type="button" class="btn btn-dark">Buy now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-12 mb-4">
+      <div class="card" style="border-radius: 15px">
+        <div
+          class="bg-image hover-overlay ripple ripple-surface ripple-surface-light"
+          data-mdb-ripple-color="light"
+        >
+          <img
+            src="https://img.freepik.com/premium-photo/wooden-bamboo-tools-eco-friendly-cleaning_107210-1210.jpg?w=360"
+            style="border-top-left-radius: 15px; border-top-right-radius: 15px"
+            class="img-fluid"
+            alt="Laptop"
+          />
+          <a href="#!">
+            <div class="mask"></div>
+          </a>
+        </div>
+        <div class="card-body pb-0">
+          <div class="d-flex justify-content-between">
+            <div>
+              <p><a href="#!" class="text-dark">Dell Xtreme 270</a></p>
+              <p class="small text-muted">Laptops</p>
+            </div>
+            <div>
+              <div
+                class="d-flex flex-row justify-content-end mt-1 mb-4 text-danger"
+              >
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+              </div>
+              <p class="small text-muted">Rated 4.0/5</p>
+            </div>
+          </div>
+        </div>
+        <hr class="my-0" />
+        <div class="card-body pb-0">
+          <div class="d-flex justify-content-between">
+            <p><a href="#!" class="text-dark">$3,999</a></p>
+            <p class="text-dark">#### 8787</p>
+          </div>
+          <p class="small text-muted">VISA Platinum</p>
+        </div>
+        <hr class="my-0" />
+        <div class="card-body">
+          <div
+            class="d-flex justify-content-between align-items-center pb-2 mb-1"
+          >
+            <a href="#!" class="text-dark fw-bold">Cancel</a>
+            <button type="button" class="btn btn-dark">Buy now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
