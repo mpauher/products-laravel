53424dbd80cb910b83d0e91100542b3288c2a2f8
Maria Paula Hernandez <mpaulah24@gmail.com>
2023-02-15 16:28:20 -0500

    Create Header Component

---
+++ products-vue/src/components/Home/HeaderHome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="text-center bg-image"
+    style="
+      background-image: url('../src/assets/header1.jpg');
+      background-size: cover;
+      height: 600px;
+    "
+  >
+    <div class="mask h-100" style="background-color: rgba(0, 0, 0, 0.6)">
+      <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+          <h1 class="mb-3">Welcome back!</h1>
+          <a class="btn btn-outline-light btn-lg" href="#!" role="button"
+            >Shop Now</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
